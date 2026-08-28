1079a413c743f298a01882a7bd7e9254e6110102
Shivam Raj <2k23.ece2311347@gmail.com>
2026-08-28 21:09:27 +0530

    Add methods to find Patient by email and phone

--- app/src/main/java/com/project/back_end/models/app/src/main/java/com/project/back_end/models/app/src/main/java/com/project/back_end/controllers/app/src/main/java/com/project/back_end/services/app/src/main/java/com/project/back_end/controllers/app/src/main/java/com/project/back_end/repo/PatientRepository.java
+++ app/src/main/java/com/project/back_end/models/app/src/main/java/com/project/back_end/models/app/src/main/java/com/project/back_end/controllers/app/src/main/java/com/project/back_end/services/app/src/main/java/com/project/back_end/controllers/app/src/main/java/com/project/back_end/repo/PatientRepository.java
@@ -5,6 +5,21 @@ import com.project.back_end.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByEmail(String email);
+
+    Optional<Patient> findByPhone(String phone);
 }
+
+
+private String phone;
+
+Optional<Patient> findByPhone(String phone);
+
+private String phoneNumber;
+
+Optional<Patient> findByPhoneNumber(String phoneNumber);
